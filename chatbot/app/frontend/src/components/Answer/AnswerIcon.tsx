3c86bde1c9ea7a92b96590c97aa8848b6d34e167
yyanghly <99605351+yyanghly@users.noreply.github.com>
2023-09-13 18:00:48 +0800

    renew

--- chatbot/app/frontend/src/components/Answer/AnswerIcon.tsx
+++
@@ -1,5 +0,0 @@
-import { Sparkle28Filled } from "@fluentui/react-icons";
-
-export const AnswerIcon = () => {
-    return <Sparkle28Filled primaryFill={"#4f943a"} aria-hidden="true" aria-label="Answer logo" />;
-};
